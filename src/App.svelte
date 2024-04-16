3e09c87473318c5d34d7821e9dc29b90424700ff
madhuri basti <SESA575434@se.com>
2024-04-16 22:50:57 +0200

    changes updated on map

--- src/App.svelte
+++ src/App.svelte
@@ -10,10 +10,22 @@
 	let chart;
 
 	onMount(() => {
+		const shapefile = require("shapefile");
 		mymap = L.map("mapid").setView([48.8534, 2.3488], 13);
 		L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 			maxZoom: 19,
 		}).addTo(mymap);
+		shapefile
+			.open("./gis_osm_places_a_free_1.shp")
+			.then((source) =>
+				source.read().then(function log(result) {
+					debugger;
+					if (result.done) return;
+					console.log(result.value);
+					return source.read().then(log);
+				}),
+			)
+			.catch((error) => console.error(error.stack));
 		calculateStats();
 		updateMap();
 	});
@@ -94,23 +106,100 @@
 	}
 </script>
 
-<div>
-	<label for="speedLimit">Speed Limit:</label>
-	<input
-		id="speedLimit"
-		type="number"
-		bind:value={speedLimit}
-		on:input={updateMap}
-	/>
-</div>
-<div id="mapid" style="height: 300px;"></div>
-{#if stats}
-	<!-- Display the statistics -->
-	<div>
-		<h2>Road Network Statistics:</h2>
-		<p>Number of Roads: {stats?.numRoads}</p>
-		<p>Average Speed Limit: {stats?.avgSpeedLimit?.toFixed(2)} km/h</p>
-		<p>Maximum Speed Limit: {stats?.maxSpeedLimit} km/h</p>
+<header class="header">
+	<img style="width:250px" src="/logo.png" alt="Logo" id="logo" />
+	<h1 id="username">User Name</h1>
+</header>
+
+<div class="test">
+	<div class="center-input">
+		<label for="speedLimit">Speed Limit:</label>
+		<input
+			id="speedLimit"
+			type="number"
+			bind:value={speedLimit}
+			on:input={updateMap}
+		/>
 	</div>
-{/if}
-<canvas id="chart"></canvas>
+
+	{#if stats}
+		<div class="dashboard">
+			<div class="card">
+				<h2>Number of Roads</h2>
+				<p>{stats?.numRoads}</p>
+			</div>
+			<div class="card">
+				<h2>Average Speed Limit</h2>
+				<p>{stats?.avgSpeedLimit?.toFixed(2)} km/h</p>
+			</div>
+			<div class="card">
+				<h2>Maximum Speed Limit</h2>
+				<p>{stats?.maxSpeedLimit} km/h</p>
+			</div>
+		</div>
+	{/if}
+	<div id="mapid" style="height: 450px;"></div>
+
+	<canvas id="chart"></canvas>
+</div>
+<footer></footer>
+
+<style>
+	.header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem;
+		background-color: #333;
+		color: white;
+	}
+	.center-input {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.test {
+		color: #fff;
+	}
+	#speedLimit {
+		width: 40%;
+		height: 40px;
+		padding: 10px;
+		border: none;
+		border-radius: 20px;
+		box-shadow: 0 0 15px 4px rgba(0, 0, 0, 0.06);
+		background: #f5f5f5;
+		outline: none;
+		font-size: 16px;
+		color: #555;
+	}
+
+	label {
+		font-size: 18px;
+		color: #555;
+	}
+	.dashboard {
+		display: flex;
+		justify-content: space-around;
+		padding: 1em;
+		background-color: #f5f5f5;
+	}
+
+	.card {
+		flex: 1;
+		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+		padding: 1em;
+		margin: 1em;
+		background-color: #fff;
+		text-align: center;
+	}
+
+	.card h2 {
+		color: #333;
+	}
+
+	.card p {
+		font-size: 2em;
+		color: #666;
+	}
+</style>
